06ccd4f8484eca300094bdc5247d26b45c8f31c4
Kanishk-VR007 <kanishkvr678@gmail.com>
2025-09-27 15:25:36 +0530

    Delete sample3 directory

--- sample3/src/Routes/way.jsx
+++
@@ -1,5 +0,0 @@
-import { lazy } from "react";
-
-export const Home = lazy( ()=> import ("./Routes/home") )
-export const About = lazy(()=> import ("./Routes/about") )
-export const Contact = lazy(()=> import("./Routes/contact"))
